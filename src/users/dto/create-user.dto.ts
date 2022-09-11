ac6ae2c570c83f0abce6a11c556130f0e599575e
Hammad-CC <haahmed@csquareonline.com>
2022-09-11 21:33:56 +0500

    create and get user APIs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  email: string;
+  name: string;
+  password_hash: string;
+  avatar?: string;
+  cnic?: string;
+  birthdate?: Date;
+  phone?: string;
+  address?: string;
+}
