adc25d4c6120e2d94cd98781db593938bd3b26fe
Hammad Javed <=>
2022-10-28 19:42:10 +0500

    add authguard in ad controller

--- src/user.decorator.ts
+++ src/user.decorator.ts
@@ -1,12 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 
 export const User = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user = request.user
-      ? request.user
-      : await(new PrismaService()).user.findFirst({ where: { id: 1 } });
-    return user;
+    return request.user;
   },
 );
