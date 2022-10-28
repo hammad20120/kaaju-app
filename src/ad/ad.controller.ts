b4a3be196d92f69bdb3a79aec211dc4aab83b142
Hammad Javed <=>
2022-10-28 19:42:10 +0500

    add authguard in ad controller

--- src/ad/ad.controller.ts
+++ src/ad/ad.controller.ts
@@ -6,7 +6,9 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { User } from 'src/user.decorator';
 import { AdService } from './ad.service';
 import { CreateAdDto } from './dto/create-ad.dto';
@@ -16,26 +18,31 @@ import { UpdateAdDto } from './dto/update-ad.dto';
 export class AdController {
   constructor(private readonly adService: AdService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createAdDto: CreateAdDto, @User() user) {
     return await this.adService.create(createAdDto, user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(@User() user) {
     return await this.adService.findAll(user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.adService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAdDto: UpdateAdDto) {
     return this.adService.update(+id, updateAdDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.adService.remove(+id);
